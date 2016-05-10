3da08ec84f14af79e0452c96d9dd6945c8534b7a
Kevin Gurden <kevin.gurden@googlemail.com>
2016-05-10 14:18:54 +0100

    minor changes

--- createSurvey.php
+++ createSurvey.php
@@ -64,7 +64,7 @@ if (!$con) {
     $total_created = 0;
     foreach ($answers as $answer) {
         error_log("answer: $answer");
-        $created = create($con, $answer.cat, $answer.id, $answer.value, $loc, $subdate, $user, $anon, $company_id);
+        $created = create($con, $answer['cat'], $answer['id'], $answer['value'], $loc, $subdate, $user, $anon, $company_id);
         if ($created) {
             $total_created +$total_created + 1;
         } else {
