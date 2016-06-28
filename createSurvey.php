70a9aba3d6f49db4fc11705f73d336d0a44425d4
Kevin Gurden <kevin.gurden@googlemail.com>
2016-06-28 15:49:46 +0100

    incremental

--- createSurvey.php
+++ createSurvey.php
@@ -26,10 +26,10 @@ include 'fn_connected.php';
 include 'fn_http_response.php';
 include 'fn_escape.php';
 
-function create($con, $cat, $id, $value, $loc, $sdate, $user, $anon, $cid) {
+function create($con, $cat, $id, $value, $loc, $sdate, $user, $anon, $cid, $refused) {
     // Issue the database create
-    $cols = "cat, id, value100, location, subdate, user, anon, company_id";
-    $vals = "'$cat', $id, '$value', '$loc', '$sdate', '$user', $anon, '$cid'";
+    $cols = "cat, id, value100, location, subdate, user, anon, company_id, refused";
+    $vals = "'$cat', $id, '$value', '$loc', '$sdate', '$user', $anon, '$cid', $refused";
 
     $insert = "INSERT INTO answers($cols) VALUES($vals)";
     $result = mysqli_query($con, $insert);
@@ -55,26 +55,31 @@ if (connected($con, $response)) {
 	$anon = got_int('anon', 0);
 	$company_id = escape($con, 'company_id', 0); // Default to 0-Unknown
 
-    if ($answers)
-
-    $total_created = 0;
-    foreach ($answers as $answer) {
-        $created = create($con, $answer['cat'], $answer['id'], $answer['value100'], $loc, $subdate, $user, $anon, $company_id);
-        if ($created) {
-            $total_created = $total_created + 1;
-        } else {
+    if (count($answers) == 0) {
+        $res = create($con, null, null, null, null, $subdate, $user, $anon, $company_id, 1); // User refused to answer
+        if (!$res) { // Error
             $response["sqlerror"] = mysqli_error($con);
         };
+    } else {
+        // Normal response
+        $total_created = 0;
+        foreach ($answers as $answer) {
+            $created = create($con, $answer['cat'], $answer['id'], $answer['value100'], $loc, $subdate, $user, $anon, $company_id, 0);
+            if ($created) {
+                $total_created = $total_created + 1;
+            } else {
+                $response["sqlerror"] = mysqli_error($con);
+            };
+        };
+        $res = $total_created == count($answers);
     };
 
-    if ($total_created == count($answers)) { // Did we successfully create all records?
+    if ($res) { // Did we successfully create all records?
         http_response_code(200);
-        // $response["status"] = 200;
         $response["message"] = "$total_created answers created";
         $response["sqlerror"] = "";
     } else { // Failure
         http_response_code(304);
-        // $response["status"] = 304;
         $response["message"] = "One or more creates failed";
         $response["sqlerror"] = mysqli_error($con);
     };
--- scoreWhistle.php
+++ scoreWhistle.php
@@ -56,15 +56,15 @@ function insert($con, $cid, $day) { // Insert a new record into 'health' or upda
     $not_closed = "status!='closed'";
     $cat_whistle = "cat='whistle'"; $cat_quick = "cat='quick'";
 
-    $whistles_open = "SELECT COUNT(*) FROM whistles WHERE company_id=$cid AND $not_closed AND $cat_whistle";
+    $whistles_open = "whistle_open = (SELECT COUNT(*) FROM whistles WHERE company_id=$cid AND $not_closed AND $cat_whistle)";
     $whistles_open_3m = "whistle_open_3m = (SELECT COUNT(*) FROM whistles WHERE company_id=$cid AND $not_closed AND $cat_whistle AND $days_90)";
     $whistles_quick_3m = "whistle_quick_3m = (SELECT COUNT(*) FROM whistles WHERE company_id=$cid AND $not_closed AND $cat_quick AND $days_90)";
-    $whistle_events = "whistle_open = ($whistles_open), whistle_open_3m = ($whistles_open_3m), whistle_quick_3m = ($whistles_quick_3m)";
+    $whistle_events = "$whistles_open, $whistles_open_3m, $whistles_quick_3m";
     
     $on_dup = "ON DUPLICATE KEY UPDATE $whistle_events";
     $insert = "INSERT INTO health SET day='$day', lookup='$lookup', company_id=$cid, $whistle_events $on_dup";
-    error_log($insert);
     $insert_result = mysqli_query($con, $insert);
+    error_log($insert, $insert_result);
     return $insert_result;
 };
 
