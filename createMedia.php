da63c953ac588732feb853326d9fd04768a1ddd7
Kevin Gurden <kevin.gurden@googlemail.com>
2016-07-18 11:44:12 +0100

    incremental

--- createMedia.php
+++ createMedia.php
@@ -38,15 +38,15 @@ if (connected($con, $response)) {
     $type = escape($con, 'type', 'photo');
 	$user = escape($con, 'user', '');
 	$cId = escape($con, 'cId', 0); // Default to 0-Unknown
-    error_log('createMedia: $type, $user, $cId');
+    error_log('createMedia: '.$type.', '.$user.', '.$cId);
     $file64 = $_POST['file64'];
 
     // Issue the database create
-    $cols = "type, file64, user, company_id";
+    $cols = "type, file, user, company_id";
     $vals = "'$type', '$file64', '$user', $cId";
 
     $insert = "INSERT INTO media($cols) VALUES($vals)";
-    error_log('createMedia: insert: $insert');
+    error_log('createMedia: insert: '.$insert);
     $result = mysqli_query($con, $insert);
     if ($result) {
         http_response_code(200);
