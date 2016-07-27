7cd6986e1262fbc3a773e475396344eb685a896b
Kevin Gurden <kevin.gurden@googlemail.com>
2016-07-27 08:06:49 +0100

    incremental

--- fn_scoring.php
+++ fn_scoring.php
@@ -218,18 +218,24 @@ function score_rolling($comp, $day_score, $other_scores) {
 //     return null;
 // };
 
-function score_event($comp, $event_name, $score, $events, $good_def, $bad_def) {
+function score_event($comp, $event_name, $score, $events, $good_def, $bad_def, $from) {
     // Return a percentage score between $good (100%) and bad (0%).
     //
     $bad = $bad_def; $good = $good_def; $value = $score[$comp.'_'.$event_name];
-    error_log($comp.'/'.$event_name.': bad_def:'.$bad_def.', good_def:'.$good_def.', value:'.$value);
+    error_log($from.' '.$comp.'/'.$event_name.': bad_def:'.$bad_def.', good_def:'.$good_def.', value:'.$value);
+    
     if (isset($events)) {
-        error_log($comp.'/'.$event_name.': events is set');
+        // error_log($comp.'/'.$event_name.': events is set');
         if (array_key_exists($event_name, $events)) {
-            error_log($comp.'/'.$event_name.': event_name:'.$event_name.' exists in events');
+            error_log($from.' '.$comp.'/'.$event_name.': event_name:'.$event_name.' exists in events');
             $bad = $events[$event_name]['low']; 
             $good = $events[$event_name]['high'];
-            error_log('score_event: '.$comp.'_'.$event_name.' is '.$bad.'/'.$good);
+            error_log($from.' '.$comp.'_'.$event_name.' is '.$bad.'/'.$good);
+        } else {
+            error_log($comp.'/'.$event_name.': event_name:'.$event_name.' is not in events');
+            foreach ($events as $key => $ev) {
+                error_log($from.' '.$comp.' key:'.$key.', event name:'.$ev['name']);
+            };
         };
     };
 
