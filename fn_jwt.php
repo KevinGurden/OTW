f51c1894cf536ebe8124ae9ba66a4251deda8aab
Kevin Gurden <kevin.gurden@googlemail.com>
2016-08-23 18:55:55 +0100

    incremental

---
+++ fn_jwt.php
@@ -0,0 +1,67 @@
+<?php
+
+function token_valid($token) {
+    $time = time();
+    $leeway = 60*60;
+    $algorithm = "HS256";
+    $secret = 'secret';
+
+    $algorithms = array('HS256'=>'sha256','HS384'=>'sha384','HS512'=>'sha512');
+    if (!isset($algorithms[$algorithm])) return false;
+    $hmac = $algorithms[$algorithm];
+    debug('hmac: '.$hmac);
+    
+    $token = explode('.',$token);
+    if (count($token)<3) return false;
+    
+    $header = json_decode(base64_decode(strtr($token[0],'-_','+/')),true);
+    debug('header: '.$header);
+    if (!$secret) return false;
+    if ($header['typ']!='JWT') return false;
+    if ($header['alg']!=$algorithm) return false;
+    debug('alg: '.$header['alg']);
+    
+    $signature = bin2hex(base64_decode(strtr($token[2],'-_','+/')));
+    if ($signature!=hash_hmac($hmac,"$token[0].$token[1]",$secret)) return false;
+    debug('sig ok');
+    
+    $claims = json_decode(base64_decode(strtr($token[1],'-_','+/')),true);
+    if (!$claims) return false;
+    if (isset($claims['nbf']) && $time+$leeway<$claims['nbf']) return false;
+    if (isset($claims['iat']) && $time+$leeway<$claims['iat']) return false;
+    if (isset($claims['exp']) && $time-$leeway>$claims['exp']) return false;
+    debug('exp: '.$claims['exp']);
+    // if (isset($claims['iat']) && !isset($claims['exp'])) {
+    //     if ($time-$leeway>$claims['iat']+$ttl) return false;
+    // };
+    debug('claims good');
+};
+
+function token() {
+    $headers = apache_request_headers();
+    if (isset($headers['Authorization'])) {
+        $auth = $headers['Authorization'];
+        debug('auth: '.$auth);
+        if (strlen($auth) >= 8) { // It's long enough to have a Bearer JWT token 
+            $token = substr($auth, 7);
+            $token_len = strlen($token);
+            debug('token length:', $token_len);
+
+            $claims = token_valid($token);
+            if ($claims == false) {
+                http_response_code(401);
+                $response["message"] = "Not authorised (1)";
+            } else {
+                return $claims;
+            };
+        } else {
+            http_response_code(401);
+            $response["message"] = "Invalid authorisation (2)";
+        };
+    } else {
+        http_response_code(401);
+        $response["message"] = "Invalid authorisation (3)";
+    };
+};
+
+?>
--- updateAccount.php
+++ updateAccount.php
@@ -22,110 +22,70 @@ header('Content-Type: application/json');
 include 'fn_connected.php';
 include 'fn_http_response.php';
 include 'fn_escape.php';
+include 'fn_jwt.php';
 include 'fn_debug.php';
-
-function verifyJWT($token, $time, $leeway, $ttl, $algorithm, $secret) {
-    $algorithms = array('HS256'=>'sha256','HS384'=>'sha384','HS512'=>'sha512');
-    if (!isset($algorithms[$algorithm])) return false;
-    $hmac = $algorithms[$algorithm];
-    debug('hmac: '.$hmac);
-    $token = explode('.',$token);
-    if (count($token)<3) return false;
-    $header = json_decode(base64_decode(strtr($token[0],'-_','+/')),true);
-    debug('header: '.$header);
-    if (!$secret) return false;
-    if ($header['typ']!='JWT') return false;
-    if ($header['alg']!=$algorithm) return false;
-    debug('alg: '.$header['alg']);
-    $signature = bin2hex(base64_decode(strtr($token[2],'-_','+/')));
-    if ($signature!=hash_hmac($hmac,"$token[0].$token[1]",$secret)) return false;
-    debug('sig ok');
-    $claims = json_decode(base64_decode(strtr($token[1],'-_','+/')),true);
-    if (!$claims) return false;
-    if (isset($claims['nbf']) && $time+$leeway<$claims['nbf']) return false;
-    if (isset($claims['iat']) && $time+$leeway<$claims['iat']) return false;
-    if (isset($claims['exp']) && $time-$leeway>$claims['exp']) return false;
-    debug('exp: '.$claims['exp']);
-    if (isset($claims['iat']) && !isset($claims['exp'])) {
-        if ($time-$leeway>$claims['iat']+$ttl) return false;
-    };
-    debug('claims good');
-    return $claims;
-};
 
 $_POST = json_decode(file_get_contents('php://input'), true);
 announce('updateAccount', $_POST); // Announce us in the log
-
-$token = null;
-$headers = apache_request_headers();
-if (isset($headers['Authorization'])) {
-    debug('got Auth in header: '.$headers['Authorization']);
-    $auth = $headers['Authorization'];
-    if (strlen($auth) >= 8) { // It's long enough to have a Bearer JWT token 
-        $token = substr($auth, 7);
-        debug('token:', $token);
-        debug('token length:', strlen($token));
-    };
-};
-
-$cls = verifyJWT($token, time(), 60*60, 60*60, 'HS256', 'secret');
-debug('claims: '.$cls);
-
-// Array for JSON response
 $response = array();
 
-// Connect to db
-$con = mysqli_connect("otw.cvgjunrhiqdt.us-west-2.rds.amazonaws.com", "techkevin", "whistleotw", "encol");
-if (connected($con, $response)) {
-    mysqli_set_charset($con, "utf8"); // Set the character set to use
+$claims = token();
+if ($claims != false) { // Token was OK
+    debug('claims: '.$cls);
 
-    // Various updates are possible
-    $new_password = $_POST['password'];
-    $new_nickname = escape($con, 'nickname', '');
-    $username = escape($con, 'username', ''); 
-    $company_id = escape($con, 'company_id', null); 
+    // Connect to db
+    $con = mysqli_connect("otw.cvgjunrhiqdt.us-west-2.rds.amazonaws.com", "techkevin", "whistleotw", "encol");
+    if (connected($con, $response)) {
+        mysqli_set_charset($con, "utf8"); // Set the character set to use
 
-    if ($username != '' && $company_id != null) { // We have enough to target the user
-        $sets = array();
-        if ($new_password != '') {
-            $sets[] = "password='".$new_password."',one_time_use=0"; // Turn off one time as well as the password has been changed
-        };
+        // Various updates are possible
+        $new_password = $_POST['password'];
+        $new_nickname = escape($con, 'nickname', '');
+        $username = escape($con, 'username', ''); 
+        $company_id = escape($con, 'company_id', null); 
 
-        if ($new_nickname != '') {
-            $sets[] = "nick='".$new_nickname."'";
-        };
-
-        // Issue the database update
-        // UPDATE users 
-        //     SET password='$new_password', nickname='$new_nickname'
-        //     WHERE username='$username' AND company_id=$company_id
-        
-        if (count($sets) > 0) {
-            $sets_comma = implode(',', $sets);
-            $update = "UPDATE users SET ".$sets_comma." WHERE username='$username' AND company_id=$company_id";
-            debug('update: '.$update);  
-            $result = mysqli_query($con, $update);
-            if ($result) { // Success
-                http_response_code(200);
-                $response["message"] = "Account updated";
-                $response["sqlerror"] = "";
-            } else { // Failure
-                error_log("$result: from $update");
-                http_response_code(402);
-                $response["message"] = "Update Account failed";
-                $response["sqlerror"] = mysqli_error($con);
+        if ($username != '' && $company_id != null) { // We have enough to target the user
+            $sets = array();
+            if ($new_password != '') {
+                $sets[] = "password='".$new_password."',one_time_use=0"; // Turn off one time as well as the password has been changed
             };
-        } else { // Success, but nothing to do
-            http_response_code(200);
-            $response["message"] = "Nothing to update";
+
+            if ($new_nickname != '') {
+                $sets[] = "nick='".$new_nickname."'";
+            };
+
+            // Issue the database update
+            // UPDATE users 
+            //     SET password='$new_password', nickname='$new_nickname'
+            //     WHERE username='$username' AND company_id=$company_id
+            
+            if (count($sets) > 0) {
+                $sets_comma = implode(',', $sets);
+                $update = "UPDATE users SET ".$sets_comma." WHERE username='$username' AND company_id=$company_id";
+                debug('update: '.$update);  
+                $result = mysqli_query($con, $update);
+                if ($result) { // Success
+                    http_response_code(200);
+                    $response["message"] = "Account updated";
+                    $response["sqlerror"] = "";
+                } else { // Failure
+                    error_log("$result: from $update");
+                    http_response_code(402);
+                    $response["message"] = "Update Account failed";
+                    $response["sqlerror"] = mysqli_error($con);
+                };
+            } else { // Success, but nothing to do
+                http_response_code(200);
+                $response["message"] = "Nothing to update";
+                $response["sqlerror"] = "";
+            };
+
+        } else { // Failure
+            debug("Need username and company id");
+            http_response_code(401);
+            $response["message"] = "Need username and company id";
             $response["sqlerror"] = "";
         };
-
-    } else { // Failure
-        debug("Need username and company id");
-        http_response_code(401);
-        $response["message"] = "Need username and company id";
-        $response["sqlerror"] = "";
     };
 };
 
