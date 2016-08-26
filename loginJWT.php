b56d621b465f101bb9ff0e554e487fa1330a09db
Kevin Gurden <kevin.gurden@googlemail.com>
2016-08-26 16:34:07 +0100

    incremental

--- loginJWT.php
+++ loginJWT.php
@@ -70,6 +70,7 @@ function pw_verify($given, $stored) { // Replace with password_verify() when PHP
 function pw_in_date($expireSQL) { // Is the temporary use-once password in date
     $expirePHP = strtotime($expireSQL);
     debug("expires: ".$expirePHP);
+    debug("time: ".time());
     return 0 <= $expirePHP;
 };
 
