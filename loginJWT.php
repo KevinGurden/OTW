c74305335d05669217620a3dfeed20ca694d4ac4
Kevin Gurden <kevin.gurden@googlemail.com>
2016-08-31 10:49:33 +0100

    incremental

--- loginJWT.php
+++ loginJWT.php
@@ -96,6 +96,8 @@ function login($given_username, $given_password, $con) {
             $password = $user_row['password'];
             $once = $user_row['one_time_use'] == 1;
             $expire = $user_row['one_time_expire'];
+            $anon_used = $user_row['anon_used'];
+            $access_code_hash = $user_row['access_code#'];
 
             if (false && checkbrute($given_username, $c_id, $con) == true) { // Check if the account is locked from too many login attempts 
                 // Account is locked. Send an email to user saying their account is locked
@@ -117,6 +119,8 @@ function login($given_username, $given_password, $con) {
                         $time = time();
                         $login_result['jwt'] = generate_token($claims, $time, $once, 'HS256', 'secret');
                         $login_result['company_id'] = $c_id;
+                        $login_result['anon_used'] = $anon_used;
+                        $login_result['access_code#'] = $access_code_hash;
                         return $login_result;
                     } else {
                         $login_result = array('result' => false, 'message' => 'Temporary password has expired');
@@ -165,6 +169,8 @@ if (connected($con, $response)) {
     $login_result = login($username, $password, $con);
     if ($login_result['result'] == true) {
         $response["jwt"] = $login_result["jwt"];
+        $response["anon_used"] = $login_result["anon_used"];
+        $response["access_code#"] = $login_result['access_code#'];
         $c_id = $login_result["company_id"];
 
         $query = "SELECT * FROM company JOIN licences ON company_id=".$c_id." WHERE id=$c_id";
