33293bd75e214005c8452acdadb125c4b06aeeea
Kevin Gurden <kevin.gurden@googlemail.com>
2016-09-07 19:27:29 +0100

    incremental

--- fn_email.php
+++ fn_email.php
@@ -13,44 +13,11 @@ function send_email($to_email, $to_name) {
 	// Create a new PHPMailer instance
 	$mail = new PHPMailer;
 
-	// Tell PHPMailer to use SMTP
-	// $mail->isSMTP();
-
-	// Enable SMTP debugging
-	// 0 = off (for production use)
-	// 1 = client messages
-	// 2 = client and server messages
-	// $mail->SMTPDebug = 2;
-
-	// Ask for HTML-friendly debug output
-	// $mail->Debugoutput = 'html';
-
-	// Set the hostname of the mail server
-	// $mail->Host = 'smtp.gmail.com';
-	// use
-	// $mail->Host = gethostbyname('smtp.gmail.com');
-	// if your network does not support SMTP over IPv6
-
-	// Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-	// $mail->Port = 587;
-
-	// Set the encryption system to use - ssl (deprecated) or tls
-	// $mail->SMTPSecure = false; // Was tls
-
-	// Whether to use SMTP authentication
-	// $mail->SMTPAuth = false;
-
-	// Username to use for SMTP authentication - use full email address for gmail
-	// $mail->Username = "kevin.gurden@googlemail.com";
-
-	// Password to use for SMTP authentication
-	// $mail->Password = "logs1Dgo";
-
 	// Set who the message is to be sent from
-	$mail->setFrom('register@rxh.not.com', 'Kevin Gurden');
+	$mail->setFrom('register@rxh.not', 'Kevin Gurden');
 
 	// Set an alternative reply-to address
-	$mail->addReplyTo('kevin.gurden@googlemail.com', 'Kevin Gurden');
+	$mail->addReplyTo('register@rxh.not', 'Kevin Gurden');
 
 	// Set who the message is to be sent to
 	$mail->addAddress($to_email, $to_name);
