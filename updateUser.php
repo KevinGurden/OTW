3d271c162fe64123d08f133404abfd03105f04b2
Kevin Gurden <kevin.gurden@googlemail.com>
2016-09-13 16:21:38 +0100

    incremental

--- updateUser.php
+++ updateUser.php
@@ -1,20 +1,21 @@
 <?php
 /*
-Create a new user in the encol database and email the new user.
+Update a user in the encol database.
 
 Security: Requires JWT "Bearer <token>" 
 
 Parameters:
-	name: User's full name. String
-	nick: User's nickname. String
-	email: User's email address. String
+	name: User's full name. Optional string
+	nick: User's nickname. Optional string
+	email: User's email address. Optional string
 	username: User's username. String
-	password: User's password. String
-	oneTime: Whether this password is a use-once: Boolean
-	expire: The date the password expires: Timestamp
-	use_encol: Permission to use. Boolean
-    use_compliance: Permission to use. Boolean
-    use_register: Permission to use. Boolean
+	password: User's password. Optional string
+	oneTime: Whether this password is a use-once: Optional boolean
+	expire: The date the password expires: Optional timestamp
+	use_encol: Permission to use. Optional boolean
+    use_comp_all: Permission to use. Optional boolean
+    use_comp_assign: Permission to use. Optional boolean
+    use_register: Permission to use. Optional boolean
 	company_id: Integer
 	debug: Turn on debug statements. Boolean
 
@@ -35,13 +36,13 @@ include 'fn_post_escape.php';
 include 'fn_jwt.php';
 include 'fn_debug.php';
 
-function doesnt_exist($con, $user, $id) { // Is this new user unique
+function does_exist($con, $user, $id) { // Is this new user unique
 	$select = "SELECT * FROM users WHERE LOWER(username)=LOWER('$user') AND company_id=$id";
     debug('select: '.$select);
     $result = mysqli_query($con, $select);
     
-    // Check for empty result
-    return $result != false && mysqli_num_rows($result) == 0;
+    // Check for result
+    return $result != false && mysqli_num_rows($result) == 1;
 };
 
 $_POST = json_decode(file_get_contents('php://input'), true);
@@ -57,46 +58,58 @@ if ($claims['result'] == true) { // Token was OK
 	    mysqli_set_charset($con, "utf8"); // Set the character set to use
 
 		// Escape the values to ensure no injection vunerability
-		$name = escape($con, 'name', '');
-		$nick = escape($con, 'nick', '');
-		$email = escape($con, 'email', ''); 
-		$username = escape($con, 'username', '');
-		$password_hash = escape($con, 'password','');
+		$name = escape($con, 'name', false);
+		$nick = escape($con, 'nick', false);
+		$email = escape($con, 'email', false); 
+		$username = escape($con, 'username', false);
+		$password_hash = escape($con, 'password', false);
 		$use_once = escape($con, 'oneTime', 1);
-		$expire_date = escape($con, 'expire', '');
-		$use_ceo360 = escape($con, 'use_ceo360', 0);
+		$expire_date = escape($con, 'expire', false);
 		$use_encol = escape($con, 'use_encol', 1);
 		$use_comp_all = escape($con, 'use_comp_all', 0);
 		$use_comp_assign = escape($con, 'use_comp_assign', 0);
 		$use_register = escape($con, 'use_register', 0);
+		$use_ceo360 = escape($con, 'use_ceo360', 0);
 		$company_id = escape($con, 'company_id', 0); // Default to 0-Unknown
 		    
-		// Check that the user doesn't already exist
-		if (doesnt_exist($con, $username, $company_id)) {
-			$cols1 = "name, nick, email, username, password, one_time_use, one_time_expire, company_id, ";
-			$vals1 = "'$name', '$nick', '$email', '$username', '$password_hash', $use_once, '$expire_date', $company_id, ";
-			$cols = $cols1."use_encol, use_comp_all, use_comp_assign, use_register, use_ceo360";
-			$vals = $vals1."$use_encol, $use_comp_all, $use_comp_assign, $use_register, $use_ceo360";
+		// Check that the user already exists
+		if (does_exist($con, $username, $company_id)) {
+			/*
+			UPDATE users SET name=$name, etc WHERE username="kg@rxh.not" AND company_id=$company_id;
+			*/
+			$sets = array();
+			if ($name != false) {$sets[] = "name='$name'"};
+			if ($nick != false) {$sets[] = "nick='$nick'"};
+			if ($email != false) {$sets[] = "email='$email'"};
+			if ($password_hash != false && $expire_date != false) {
+				$sets[] = "password='$password_hash'";
+				$sets[] = "one_time_use=$use_once";
+				$sets[] = "one_time_expire='$expire_date'"};
+			};
+			$sets[] = "use_comp_all=$use_comp_all"; $sets[] = "use_comp_assign=$use_comp_assign";
+			$sets[] = "use_encol=$use_encol"; $sets[] = "use_register=$use_register"; $sets[] = "use_ceo360=$use_ceo360";
+
+			$sets_comma = implode(',', $sets);
 			
-			$insert = "INSERT INTO users($cols) VALUES($vals)";
-			$result = mysqli_query($con, $insert);
-			debug("insert: ".$insert);
+			$update = "UPDATE users SET $sets_comma WHERE username='$username' AND company_id=$company_id";
+			$update = mysqli_query($con, $update);
+			debug("update: ".$update);
 
 			if ($result) { // Success
 		        http_response_code(200);
-		        $response["message"] = "User created";
+		        $response["message"] = "User updated";
 		        $response["id"] = mysqli_insert_id($con); // Return the id of the record added
 		        $response["sqlerror"] = "";
 			} else { // Failure
 		        http_response_code(402);
 		        debug("insert result: ".mysqli_error($con));
-		        $response["message"] = "Create user failed";
+		        $response["message"] = "Update user failed";
 		        $response["sqlerror"] = mysqli_error($con);
 		    };
 		} else { // User already exists
 			http_response_code(409);
-	        debug("User exists already");
-	        $response["message"] = "User name is taken";
+	        debug("User not found");
+	        $response["message"] = "User name not found";
 	        $response["sqlerror"] = "";
 	    };
 	};
